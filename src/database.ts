1715b1dd4448a4347641f1cf9701fd7ab59e91a7
Alexandre Moraes <a.moraesdocarmo@gmail.com>
2023-02-10 06:46:36 -0300

    Add: config create migrate

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,11 @@
 import { knex as setupkex } from 'knex'
 
-export const knex = setupkex({
+export const config = {
   client: 'sqlite',
   connection: {
     filename: './tmp/app.db',
   },
-})
+  useNullAsDefault: true,
+}
+
+export const knex = setupkex(config)
