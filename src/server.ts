b828293d0e7afcedf4d1ca010efd816850eb03b1
Alexandre Moraes <a.moraesdocarmo@gmail.com>
2023-02-16 07:04:54 -0300

    add: create transaction route

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,14 @@
 import fastify from 'fastify'
 
 import { env } from './env'
-import { knex } from './database/configs'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const tables = knex('sqlite_schema').select('*')
-
-  return tables
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
-app
-  .listen({
-    port: env.PORT,
-  })
-  .then(() => {
-    console.log('HTTP Server Running!')
-  })
+app.listen({ port: env.PORT }).then(() => {
+  console.log('HTTP Server Running!')
+})
