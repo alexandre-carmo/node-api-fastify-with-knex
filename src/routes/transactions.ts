9d8d8100367809dceff7c1692ee5089ebe6a1450
Alexandre Moraes <a.moraesdocarmo@gmail.com>
2023-02-28 06:19:05 -0300

    Mapping tables knex

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,6 +5,36 @@ import { randomUUID } from 'node:crypto'
 import { knex } from '../database/configs'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamSchema.parse(request)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return { summary }
+  })
+
   app.post('/', async (request, replay) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
