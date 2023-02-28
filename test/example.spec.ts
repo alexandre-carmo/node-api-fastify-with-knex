fd16d771154857e006d66d08efa02e0090176044
Alexandre Moraes <a.moraesdocarmo@gmail.com>
2023-02-28 07:31:13 -0300

    Add category test

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,15 +1,4 @@
-import { afterAll, beforeAll, expect, test } from 'vitest'
-import { app } from '../src/app'
-
-import request from 'supertest'
-
-beforeAll(async () => {
-  await app.ready()
-})
-
-afterAll(async () => {
-  await app.close()
-})
+import { expect, test } from 'vitest'
 
 test('Description test', () => {
   // operation
@@ -17,15 +6,4 @@ test('Description test', () => {
 
   // validation
   expect(responseStatusCode).equal(200)
-})
-
-test('user can create a new transaction', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transaction',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201)
 })
