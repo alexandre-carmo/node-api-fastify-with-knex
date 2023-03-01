dff9664c091170471962ba5cdccd9bd851b98f67
Alexandre Moraes <a.moraesdocarmo@gmail.com>
2023-03-01 06:44:06 -0300

    Add environment test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,7 +1,8 @@
-import { afterAll, beforeAll, describe, it } from 'vitest'
 import { app } from '../src/app'
+import { afterAll, beforeAll, describe, expect, it, beforeEach } from 'vitest'
 
 import request from 'supertest'
+import { execSync } from 'node:child_process'
 
 describe('Transactions routes', () => {
   beforeAll(async () => {
@@ -10,6 +11,12 @@ describe('Transactions routes', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+
+  // for each test run
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
   })
 
   it('should be able to create a new transaction', async () => {
@@ -21,5 +28,29 @@ describe('Transactions routes', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })
